b93f5651b3ca8aad24af9f9cfd981b7eff61c832
Sanju2op <120462833+Sanju2op@users.noreply.github.com>
2025-10-15 08:56:46 +0530

    admin single group management guide mobile not fetching from api solved

--- client/guide/vite.config.js
+++ client/guide/vite.config.js
@@ -1,20 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    port: 3001
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3001,
   },
-})
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  // server: {
+  //   port: 3001,
+  // },
+});
